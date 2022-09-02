e15f691fd4bc980432c792dbba7af54f08c39215
hostlife22 <sima.serafim2021@mail.ru>
2022-09-02 14:07:48 +0300

    init: remix jokes app

---
+++ app/root.tsx
@@ -0,0 +1,16 @@
+import { LiveReload } from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <title>Remix Jokes App</title>
+      </head>
+      <body>
+        <h1>Hello World!!</h1>
+        {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
+      </body>
+    </html>
+  );
+}
